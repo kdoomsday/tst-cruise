810e04ad4ea6ae0a03835467a4f9631f12788791
Eduardo Barrientos <doomsday@dell-i>
2025-09-01 00:33:19 -0400

    Make Problem 1 runner accept custom rates and prices files

--- README.md
+++ README.md
@@ -17,8 +17,16 @@ All input data is in `src/main/resources`
 ## Running the application
 Each problem can be run on its own:
 
+### Problem 1
 ```bash
 sbt "runMain Problem1"    # Print all Best Group Prices
+sbt "runMain Problem1 <ratesFile>" # Best Group Prices using rates from <ratesFile>
+sbt "runMain Problem1 <ratesFile> <cabinPricesFile>" # Best Group Prices using rates and cabin prices from custom files
+```
+
+### Problem 2
+Run using default data files
+```bash
 sbt "runMain Problem2"    # Print all PromotionCombos, combos for P1, and combos for P3
 sbt "runMain Problem2 P2" # Print only combos for P2
 ```
--- src/main/scala/Problem1.scala
+++ src/main/scala/Problem1.scala
@@ -7,12 +7,33 @@ import tst.models.Rate
 
 
 object Problem1 {
+
   def main(args: Array[String]): Unit = {
     // For read errors in this case we want the exception
-    val rates       = Readers.rates("data/rates.txt").get
-    val cabinPrices = Readers.cabinPrices("data/cabin-prices.txt").get
+    val (ratesFile, cabinPricesFile) = loadFiles(args)
+
+    val rates       = Readers.rates(ratesFile).get
+    val cabinPrices = Readers.cabinPrices(cabinPricesFile).get
 
     val solver: BestPriceSolver = BestPriceSolverImpl
     solver.getBestGroupPrices(rates, cabinPrices).foreach(println)
   }
+
+
+  /**
+   * Choose files to load given program arguments.
+   * Usage `runMain Problem1 [<ratesFile>] [<cabinPricesFile>]`
+   *
+   * If the rates file is not specified, defaults to "data/rates.txt"
+   * If the cabin prices file is not specified, defaults to "data/cabin-prices.txt"
+   *
+   * @param args Program arguments
+   * @return Filename for rates and filename for cabin prices
+   */
+  private def loadFiles(args: Array[String]): (String, String) = {
+    val ratesFile       = args.headOption.getOrElse("data/rates.txt")
+    val cabinPricesFile = args.drop(1).headOption.getOrElse("data/cabin-prices.txt")
+    (ratesFile, cabinPricesFile)
+  }
+
 }
